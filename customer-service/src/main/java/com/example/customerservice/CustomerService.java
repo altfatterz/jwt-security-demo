34482c7852c49fc6f14833601f2f5d9f46bdfd44
Zoltan Altfatter <zoltan.altfatter@mimacom.com>
2018-07-06 17:00:27 +0200

    add customer-service

--- customer-service/src/main/java/com/example/customerservice/CustomerService.java
+++ customer-service/src/main/java/com/example/customerservice/CustomerService.java
@@ -1,7 +1,15 @@
 package com.example.customerservice;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class CustomerService {
@@ -9,4 +17,23 @@ public class CustomerService {
     public static void main(String[] args) {
         SpringApplication.run(CustomerService.class, args);
     }
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Customer {
+
+    private String firstName, lastName;
+
+}
+
+@RestController
+class CustomerRestController {
+
+    @RequestMapping("/customers")
+    public List<Customer> customers() {
+        return Arrays.asList(new Customer("John", "Doe"));
+    }
+
 }
